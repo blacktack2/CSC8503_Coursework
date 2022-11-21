5b13d52d58ec29db027c43c653924679101f3232
b9050231 <b9050231@newcastle.ac.uk>
2022-11-21 14:18:17 +0000

    Fixed inverted y movement and added sprint

--- NCLCoreClasses/Camera.cpp
+++ NCLCoreClasses/Camera.cpp
@@ -25,7 +25,7 @@ void Camera::UpdateCamera(float dt) {
 		yaw -= 360.0f;
 	}
 
-	float frameSpeed = 100 * dt;
+	float frameSpeed = (Window::GetKeyboard()->KeyDown(KeyboardKeys::CONTROL) ? 200 : 50) * dt;
 
 	if (Window::GetKeyboard()->KeyDown(KeyboardKeys::W)) {
 		position += Matrix4::Rotation(yaw, Vector3(0, 1, 0)) * Vector3(0, 0, -1) * frameSpeed;
@@ -42,10 +42,10 @@ void Camera::UpdateCamera(float dt) {
 	}
 
 	if (Window::GetKeyboard()->KeyDown(KeyboardKeys::SHIFT)) {
-		position.y += frameSpeed;
+		position.y -= frameSpeed;
 	}
 	if (Window::GetKeyboard()->KeyDown(KeyboardKeys::SPACE)) {
-		position.y -= frameSpeed;
+		position.y += frameSpeed;
 	}
 }
 
