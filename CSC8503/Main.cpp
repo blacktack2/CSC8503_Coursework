dac011a23c1e0105a9a5c9b9a7599469a68ebb48
b9050231 <b9050231@newcastle.ac.uk>
2022-11-30 13:21:51 +0000

    Added pathfinding test

--- CSC8503/Main.cpp
+++ CSC8503/Main.cpp
@@ -61,10 +61,31 @@ void TestStateMachine() {
 	}
 }
 
+static std::vector<Vector3> testNodes;
+
 void TestPathfinding() {
+	NavigationGrid grid("TestGrid1.txt");
+
+	NavigationPath outPath;
+
+	Vector3 startPos(70, 0, 10);
+	Vector3 endPos(60, 0, 80);
+
+	bool found = grid.FindPath(startPos, endPos, outPath);
+
+	Vector3 pos;
+	while (outPath.PopWaypoint(pos)) {
+		testNodes.push_back(pos);
+	}
 }
 
 void DisplayPathfinding() {
+	for (int i = 1; i < testNodes.size(); i++) {
+		Vector3 a = testNodes[i - 1];
+		Vector3 b = testNodes[i];
+
+		Debug::DrawLine(a, b, Vector4(0, 1, 0, 1));
+	}
 }
 
 /*
@@ -81,6 +102,7 @@ hide or show the
 */
 int main() {
 	//TestStateMachine();
+	TestPathfinding();
 
 	Window*w = Window::CreateGameWindow("CSC8503 Game technology!", 1280, 720);
 
@@ -113,6 +135,7 @@ int main() {
 		w->SetTitle("Gametech frame time:" + std::to_string(1000.0f * dt));
 
 		g->UpdateGame(dt);
+		DisplayPathfinding();
 	}
 	Window::DestroyGameWindow();
 }
