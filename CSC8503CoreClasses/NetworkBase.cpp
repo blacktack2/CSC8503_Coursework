86f291bb92730575b249451ff3fb2311a837785d
b9050231 <b9050231@newcastle.ac.uk>
2022-12-02 12:04:47 +0000

    Implemented Networking

--- CSC8503CoreClasses/NetworkBase.cpp
+++ CSC8503CoreClasses/NetworkBase.cpp
@@ -19,5 +19,16 @@ void NetworkBase::Destroy() {
 }
 
 bool NetworkBase::ProcessPacket(GamePacket* packet, int peerID) {
+	PacketHandlerIterator first,  last;
+	bool canHandle = GetPacketHandlers(packet->type, first, last);
+
+	if (canHandle) {
+		for (auto i = first; i != last; i++) {
+			i->second->ReceivePacket(packet->type, packet, peerID);
+		}
+		return true;
+	}
+
+	std::cout << __FUNCTION__ << " no handler for packet type " << packet->type << "\n";
 	return false;
 }
