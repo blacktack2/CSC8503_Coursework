6d84ab0198aeb3906795153877321ca33082f407
b9050231 <b9050231@newcastle.ac.uk>
2022-12-16 10:15:49 +0000

    NPC will now flee from player and goose

--- CSC8503/NPCObject.h
+++ CSC8503/NPCObject.h
@@ -12,16 +12,22 @@ namespace NCL {
 			~NPCObject();
 
 			virtual void Update(float dt) override;
-
-			void Wander(float dt);
 		private:
+			void Wander(float dt);
+			void Flee(float dt);
+
 			StateMachine stateMachine;
 
+			GameObject* perceptionTrigger;
+
 			OrientationConstraint* orientationConstraint;
+
+			GameObject* objectOfFear = nullptr;
 
 			Vector3 wanderDir = Vector3(0, 0, -1);
 
 			const float wanderForce = 100.0f;
+			const float fleeForce = 150.0f;
 		};
 	}
 }
